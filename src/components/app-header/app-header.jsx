f478489a3fc043c474e1a578d71c75598c51c826
l.kozlachkova <l.kozlachkova@tinkoff.ru>
2021-11-06 12:56:12 +0300

    init

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,14 @@
+import React, {} from 'react';
+import styles from './app-header.module.css';
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Icons } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Typography } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Box } from '@ya.praktikum/react-developer-burger-ui-components';
+
+const AppHeader = ({}) => {
+    return (
+        <Logo/>
+    );
+};
+
+export default AppHeader;
