5f2c13d8599f0ca1e89988e215aef16f4949b0a9
l.kozlachkova <l.kozlachkova@tinkoff.ru>
2021-11-16 18:23:24 +0300

    progress - header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import './App.css';
+import styles from './app.module.css';
 import AppHeader from "./components/app-header/app-header";
 import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
 
 function App() {
+    const titleStyles = `text text_type_main-large pt-4 ${styles.title}`;
+    const contentStyles = `${styles.content}`;
   return (
     <div className="App">
       <AppHeader/>
-      <BurgerIngredients/>
+        <div className={contentStyles}>
+            <h1 className={titleStyles}>Соберите бургер</h1>
+            <BurgerIngredients/>
+        </div>
     </div>
   );
 }
--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -4,29 +4,34 @@ import {Logo} from '@ya.praktikum/react-developer-burger-ui-components';
 import {BurgerIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import {ListIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import {ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
-import {Typography} from '@ya.praktikum/react-developer-burger-ui-components';
-import {Box} from '@ya.praktikum/react-developer-burger-ui-components';
+
 
 const AppHeader = ({}) => {
+    const headerStyles = `mb-5 ${styles.header}`
+    const navStyles = `${styles.nav}`
+    const itemStyles = `pr-5 pl-5 ${styles.navItem}`
+    const logoStyles = `pt-4 pr-5 pb-4 pl-5 ${styles.logo}`
     return (
-        <header className='m-5' style={{display: 'flex'}}>
-            <nav className='p-5'  style={{display: 'flex'}}>
-                <h2  className="pt-4 pr-5 pb-4 pl-5" >
-                    <BurgerIcon type="primary"/>
-                    <span className="text text_type_main-default pl-2">Конструктор</span>
-                </h2>
-                <h2  className={styles.navItem}>
-                    <ListIcon type="primary"/>
-                    <span  className="text text_type_main-default pl-2">Лента заказов</span>
-                </h2>
-                <h2  className={styles.navItem}>
+        <header className={headerStyles}>
+            <div className={styles.content}>
+                <nav className={navStyles}>
+                    <h2  className={itemStyles} >
+                        <BurgerIcon type="primary"/>
+                        <span className="text text_type_main-default pl-2">Конструктор</span>
+                    </h2>
+                    <h2  className={itemStyles}>
+                        <ListIcon type="primary"/>
+                        <span  className="text text_type_main-default pl-2">Лента заказов</span>
+                    </h2>
+                </nav>
+                <h2  className={logoStyles}>
                     <Logo/>
                 </h2>
-            </nav>
-            <h2 className={styles.login}>
-                <ProfileIcon type="primary"/>
-                <span  className="text text_type_main-default pl-2">Личный кабинет</span>
-            </h2>
+                <h2 className={styles.login}>
+                    <ProfileIcon type="primary"/>
+                    <span  className="text text_type_main-default pl-2">Личный кабинет</span>
+                </h2>
+            </div>
         </header>
 
     );
